16e83e39793d73cd726edba6411d25d346760e65
SumireMaki <makisumire0617@gmail.com>
2024-01-04 22:21:44 +0900

    B096:爆弾の大爆発[1]

---
+++ B/b096.php
@@ -0,0 +1,11 @@
+<?php
+    // 標準入力を一行ずつ配列に代入
+    while ($stdin = trim(fgets(STDIN))) {
+        $stdin_array[] = $stdin;
+    }
+    
+    // h行 w列
+    list($h, $w) = explode(' ', $stdin_array[0]);
+    
+    // 全マス目
+    $all_square = $h * $w;
