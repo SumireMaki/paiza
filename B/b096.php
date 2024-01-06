97d598074af8ea2d69e39ec4a82b8e959bb9a790
SumireMaki <makisumire0617@gmail.com>
2024-01-06 23:28:50 +0900

    B096:爆弾の大爆発[4]

--- B/b096.php
+++ B/b096.php
@@ -1,30 +1,39 @@
 <?php
-// B096:爆弾の大爆発
-    // 標準入力を一行ずつ配列に代入
+    // 標準入力を$stdin_arrayに格納
     while ($stdin = trim(fgets(STDIN))) {
         $stdin_array[] = $stdin;
     }
     
-    for ($i = 0; $i = 0; $i++) {
-        
-    }
-    // h行 w列
+    // $stdin_array[0] h行 w列
     list($h, $w) = explode(' ', $stdin_array[0]);
-    $burst_info = array_slice(0, $stdin_array);
     
+    // $stdin_array[1]以降を爆発情報として格納
+    $burst_info = [];
+    for ($i = 1; $i <= $stdin_array[0]; $i++) {
+        $burst_info[] = explode(' ', $stdin_array[$i]);
+    }
+
     // 全マス目
     $all_square = $h * $w;
     
     // 爆発したマス目
     $minus = 0;
-
+    
     //i行目j列目
-    for ($i = 0; $i = $h; $i++) {
-        for($j = 0; $j <= $w; $j++){
-            if(strpos($stdin_array[$i],'#') !== false || strpos($stdin_array[$j],'#') !== false){
+    for ($i = 0; $i < $h; $i++) {
+        for($j = 0; $j < $w; $j++){
+            if(strpos(implode('', $burst_info[$i]), '#') !== false){
                 $minus += ($h - $i) + ($w - $j) - 1;
             }
         }
+    }
+    
+    // 配列[i]と[i+1]を比較
+        for ($i = 0; $i < $burst_info; $i++) {
+            if(strpos(implode('', $burst_info[$i]), '#') !== false){
+                $burst_info[$i] = '####';
+                $burst_info[$i+1] = 
+            }
     }
     
     var_dump($minus);
